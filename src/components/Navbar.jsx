88296209085b500f3e6061aaaa1fd05175d4004b
Khitishnayak <kknayak049@gmail.com>
2025-09-28 21:45:07 +0530

    Revert "Fix mobile responsiveness: optimize 3D canvas, improve spacing, enhance navigation"
    This reverts commit a3b89db29b8882654f49f9ce5feccd8b17bd41db.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,20 +67,20 @@ const Navbar = () => {
           <img
             src={toggle ? close : menu}
             alt='menu'
-            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            className='w-[28px] h-[28px] object-contain'
             onClick={() => setToggle(!toggle)}
           />
 
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[160px] z-10 rounded-xl`}
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-6'>
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
-                  className={`font-poppins font-medium cursor-pointer text-[18px] py-2 ${
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
                     active === nav.title ? "text-white" : "text-secondary"
                   }`}
                   onClick={() => {
